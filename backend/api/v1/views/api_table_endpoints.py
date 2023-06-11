958628d3b36846e948e5f7b29aed1cdbd2182dac
peter <petersp2000@gmail.com>
2023-06-11 15:53:32 +0100

    fully functional user models and parameters update

--- backend/api/v1/views/api_table_endpoints.py
+++ backend/api/v1/views/api_table_endpoints.py
@@ -52,6 +52,8 @@ def create_model(user, api_id):
         param_dt_length = param.get("dt_length")
         constraints = param.get("constraints") or []
         try:
+            if TableParameter.query.filter_by(name=param_name, table_id=new_table.id).first():
+                return jsonify({"error": "name of table param already exist for this table, it must be unique"})
             p = TableParameter(name=param_name, data_type=param_dt, dataType_length=param_dt_length, table_id=new_table.id)
             for const in constraints:
                 get_c = Constraint.query.filter_by(name=const).first()
@@ -60,6 +62,7 @@ def create_model(user, api_id):
                 else:
                     p.constraints.append(Constraint(name=const))
             db.session.add(p)
+            db.session.commit()
         except Exception as e:
             print(e)
             return jsonify({"error": "Something went wrong"}), 400
@@ -74,16 +77,50 @@ def update_model(user, api_id, model_id):
     data = request.get_json()
     name = data.get('name')
     description = data.get('description')
+    table_parameters = data.get('tbl_params') or []
     api = Api.query.filter_by(id=api_id, user_id=user.id).first()
     if not api:
         return jsonify({"error": "no api of such is associated to the user"})
     get_table = Table.query.filter_by(id=model_id, api_id=api_id).first()
     if not get_table:
         return jsonify({"error": "Table doesn't exist"}), 400
+    if get_table.entry_lists:
+        return jsonify({"error": "You cannot the table when it already has data"}), 400
     if name:
         get_table.name = name
     if description:
         get_table.description = description
+    
+    for param in table_parameters:
+        param_id = param.get("id")
+        param_name = param.get("name")
+        param_dt = param.get("datatype")
+        param_dt_length = param.get("dt_length")
+        constraints = param.get("constraints") or []
+        if not param_id:
+            continue
+        try:
+            p = TableParameter.query.filter_by(id=param_id, table_id=get_table.id).first()  
+            if not p:
+                continue
+            if param_name:
+                if not TableParameter.query.filter_by(name=param_name, table_id=get_table.id).first():
+                    p.name = param_name
+            if param_dt:
+                p.data_type = param_dt
+            if param_dt_length:
+                p.dataType_length = param_dt_length
+            for const in constraints:
+                get_c = Constraint.query.filter_by(name=const).first()
+                if get_c:
+                    p.constraints.append(get_c)
+                else:
+                    p.constraints.append(Constraint(name=const))
+            db.session.add(p)
+            db.session.commit()
+        except Exception as e:
+            print(e)
+            return jsonify({"error": "Something went wrong"}), 400
     db.session.commit()
     return jsonify({"id": get_table.id, "name": get_table.name, "desc": get_table.description})
 
